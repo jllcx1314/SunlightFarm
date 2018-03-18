84d4af45f4a6d2ba3763efc1a4d47d408c2d8821
日光农场 <sunlightfarm@riguangnongchangdemacbook-air.local>
2018-03-18 08:39:06 +0800

    创建了Cat 类

---
+++ sun/sun/Cat.h
@@ -0,0 +1,13 @@
+//
+//  Cat.h
+//  sun
+//
+//  Created by 日光农场 on 2018/3/18.
+//  Copyright © 2018年 日光农场. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Cat : NSObject
+
+@end
---
+++ sun/sun/Cat.m
@@ -0,0 +1,13 @@
+//
+//  Cat.m
+//  sun
+//
+//  Created by 日光农场 on 2018/3/18.
+//  Copyright © 2018年 日光农场. All rights reserved.
+//
+
+#import "Cat.h"
+
+@implementation Cat
+
+@end
