6f47b264f60fdd424abfd4369742a64ad1222412
日光农场 <sunlightfarm@riguangnongchangdemacbook-air.local>
2018-03-18 08:37:13 +0800

    开始开发

--- sun/sun/ViewController.m
+++ sun/sun/ViewController.m
@@ -16,14 +16,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"开始开发");
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 
 @end
